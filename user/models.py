2c6cb46473b986c7b6506c6eeb8d4e90e09a09db
Min-Ho Lee <minho.lee0716@gmail.com>
2020-09-26 16:35:23 +0900

    [ User App ] - models.py 작성 - time_zone 해결(초 단위 소수점 문제는 해결 못함)

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_no     = models.AutoField(primary_key=True)
+    name        = models.CharField(max_length=50, null=False)
+    email       = models.EmailField(max_length=255, null=False)
+    password    = models.CharField(max_length=50, null=False)
+    is_deleted  = models.BooleanField(default=False)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    last_access = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table='users'
